3bb8f46bd7154aacb38dfedaa16d8139d06e9447
xiuzhi.shang <xiuzhi.shang@qiyuesuo.com>
2016-12-16 16:10:34 +0800

    add some explain

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# boke
+# nodeapp
 
 自定义的nodejs express项目，包含了mysql 数据库连接等一些操作，页面相对简单，功能已经实现。
 
@@ -9,4 +9,13 @@ export NODE_ENV=production
 #配置端口号
 npm config set nodeapp:port 8080
 
+
+#配置debug工具
+采用的是node inspector
+
+（1）安装 npm install node-inspector -g
+
+（2）项目根目录下输入node-inspector命令
+
+（3）项目根目录下另开一个命令窗口npm start。（只针对本项目）
 
--- module/config/mysqlPool.js
+++ module/config/mysqlPool.js
@@ -1,15 +1,29 @@
 var mysql = require('mysql');
+var option = null;
+if(process.env.NODE_ENV === 'production'){ //配置生产环境数据库参数
+	option = {                            //配置生产环境数据库参数
+		host: '192.168.0.145', 
+		user: 'qiyuesuo',
+		password: 'qiyuesuo',
+		database:'test', 
+		port: 3306,
+		connectionLimit: 10,
+		waitForConnections: false
+	}
+}else if(process.env.NODE_ENV === 'release'){//release环境
 
-// 使用连接池
-var option = {
-	host: '*****', 
-	user: '*****',
-	password: '*******',
-	database:'test', 
-	port: 3306,
-	connectionLimit: 10,
-	waitForConnections: false
+}else{
+	option = {                            //配置生产环境数据库参数
+		host: '192.168.0.145', 
+		user: 'qiyuesuo',
+		password: 'qiyuesuo',
+		database:'test', 
+		port: 3306,
+		connectionLimit: 10,
+		waitForConnections: false
+	}
 }
 
-var pool  = mysql.createPool(option);
+// 使用连接池
+var pool = mysql.createPool(option);
 module.exports = pool
